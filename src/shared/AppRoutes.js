4a525602453b9303a2e5fdb2ba3092b085a8d177
abdur-rehman-ali <alijoyia911@gmail.com>
2022-09-22 14:22:59 +0500

    Lazy loading

--- src/shared/AppRoutes.js
+++ src/shared/AppRoutes.js
@@ -4,17 +4,27 @@ import { FeaturedProducts } from '../components/Products/FeaturedProducts'
 import { NewProducts } from '../components/Products/NewProducts'
 import { Product } from '../components/Products/Product'
 import { Products } from '../components/Products/Products'
-import { About } from '../components/shared/About'
+// import { About } from '../components/shared/About'
 import { Home } from '../components/shared/Home'
+import Loading from '../components/shared/Loading'
 import { Users } from '../components/Users/Users'
 import { PageNotFound } from './PageNotFound'
+const LazyAbout = React.lazy((() => import('../components/shared/About.js')))
 
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
+      {/* <Route path='about' element={<About />} /> */}
+      <Route
+        path='about'
+        element={
+          <React.Suspense fallback={<Loading/>}>
+            <LazyAbout />
+          </React.Suspense>
+        }
+      />
       <Route path='users' element={<Users />} />
       <Route path='products' element={<Products />}>
         <Route index element={<NewProducts />} />
